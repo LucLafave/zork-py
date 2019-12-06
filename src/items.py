0679822af83b60acd8c0035c65e6b4ca0f01b8a1
LucLafave <luclafave@mymail.mines.edu>
2019-12-05 20:55:26 -0700

    fixing the items python code with correct functions

--- src/items.py
+++ src/items.py
@@ -1,14 +1,20 @@
 # Items that exist in each room and the functions you can use to interact with them
 
 #TODO Define items in each room
-room0items = []
-room1items = []
+room0items = ['leaflet']
+room1items = ['fish']
 room2items = []
 room3items = []
 room4items = []
-# ...
+room5items = []
+room6items = []
+room7items = ['lantern']
+room8items = []
+room9items = []
+room10items = []
+room11items = ['longsword']
 
-allItems = [room0items, room1items, room2items, room3items, room4items]
+allItems = [room0items, room1items, room2items, room3items, room4items,room5items,room6items,room7items,room8items,room9items,room10items,room11items]
 
 # Returns item to be added to inventory if it exists in this room and removes it from the list of items in the coom
 def pick_up(itemName, roomNum):
@@ -24,6 +30,15 @@ def pick_up(itemName, roomNum):
 def put_down(itemName, roomNum):
     print("You put down the ", itemName)
     # TODO Add item to room item list
+    allItems[roomNum].append(itemName) 
     return 0
 
-# def useItem()
+def useItem(itemName,itemList):
+    if itemName in itemList:
+        print("You used the ", itemName)
+        return True
+    else:
+        print('You do no have that item in inventory ')
+        return False
+def printroom():
+    print(room0items)
